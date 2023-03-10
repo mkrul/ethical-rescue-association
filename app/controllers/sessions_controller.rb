5cd6fdd91bdd5a1882716525fc75add5bbef7c9d
mkrul <mishakrul@gmail.com>
2023-03-10 07:16:58 -0500

    remove byebug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,7 @@
-require 'byebug'
+
 class SessionsController < ApplicationController
 
   def destroy
-    byebug
-    session[:user_id] = nil
-    redirect_to root_path, notice: 'Logged out'
   end
 
 end
