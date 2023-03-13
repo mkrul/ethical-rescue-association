c463ab2dd33ea978ba19edb8ed77e2ec2f987166
mkrul <mishakrul@gmail.com>
2023-03-13 06:56:07 -0400

    add user mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,7 @@
+class UserMailer < ApplicationMailer
+  default from: 'support@ethicalrescueassociation.com'
+
+  def welcome_email
+
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_create :send_sign_up_confirmation_email
+
+  def send_sign_up_confirmation_email
+  end
+
 end
