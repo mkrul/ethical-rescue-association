ab1492d7f0ccf5745f41b65d5e2d001510eb59b4
mkrul <mishakrul@gmail.com>
2023-03-13 09:08:28 -0400

    add contacts

---
+++ db/migrate/20230313114239_add_contacts.rb
@@ -0,0 +1,12 @@
+class AddContacts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :contacts do |t|
+      t.string :first_name, default: nil
+      t.string :last_name, default: nil
+      t.string :home_phone, default: nil
+      t.string :cell_phone, default: nil
+      t.string :work_phone, default: nil
+      t.string :email, default: nil
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_11_182006) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_13_114239) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "home_phone"
+    t.string "cell_phone"
+    t.string "work_phone"
+    t.string "email"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
