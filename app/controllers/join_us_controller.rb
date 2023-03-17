e594c4867a9e190d7967271d1e11352604460512
mkrul <mishakrul@gmail.com>
2023-03-17 14:33:21 -0400

    add join us banner

---
+++ app/controllers/join_us_controller.rb
@@ -0,0 +1,7 @@
+class JoinUsController < ApplicationController
+
+  def index
+
+  end
+
+end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 class MainController < ApplicationController
   include MainHelper
+
   def index
-    # debugger
 
   end
 
