61b35e699c99da21f5181aebf183895d8039d9d7
mkrul <mishakrul@gmail.com>
2023-08-07 09:34:57 -0400

    emails are working in dev

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,15 @@
 class UserMailer < ApplicationMailer
-  default from: 'support@ethicalrescueassociation.com'
+  default from: 'ethicalrescueassociation@gmail.com'
+  layout "mailer"
 
+  def mailer(user)
+    @name = 'derp'
+    @email = 'derp@derp.com'
+    @message = 'herp derp'
+    mail(
+      to: 'ethicalrescueassociation@gmail.com',
+      subject: "New email inquiry from #{@name} (#{@email})",
+      body: @message
+    )
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,8 +38,21 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  host = "localhost:3000"
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            'ethicalrescueassociation@gmail.com',
+    password:             'ycfffufpfgxfnfqw',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
+  # UserMailer.mailer(User.first).deliver!
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
