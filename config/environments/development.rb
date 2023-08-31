84fe9d85e2b7ea8c3974eb287c6f772fde2c4de3
mkrul <mishakrul@gmail.com>
2023-08-31 08:15:33 -0400

    unuck development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,8 +14,6 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  config.assets.compile = true
-
   # Enable server timing
   config.server_timing = true
 
