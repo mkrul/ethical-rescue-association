93bc163e40e1d106cd552b274520635bf9f8d38c
mkrul <mishakrul@gmail.com>
2023-09-11 15:26:45 -0400

    get headers

--- app/controllers/contact_us_controller.rb
+++ app/controllers/contact_us_controller.rb
@@ -9,49 +9,14 @@ class ContactUsController < ApplicationController
       flash[:error] = "Your name, email, and a message are required. Please try again."
       redirect_to contact_us_path
     else
-      remote_ip = request.remote_ip
-      x_forwarded_for = request.env["HTTP_X_FORWARDED_FOR"]
+      headers = request.env.headers
 
-      begin
-        remote_ip_results = Geocoder.search(remote_ip)
-        x_forwarded_for_results = Geocoder.search(x_forwarded_for)
-        remote_lat = remote_ip_results&.first&.coordinates&.first
-        remote_long = remote_ip_results&.first&.coordinates&.second
-
-        x_lat = x_forwarded_for_results&.first&.coordinates&.first
-        x_long = x_forwarded_for_results&.first&.coordinates&.second
-
-        remote_geo_localization = "#{remote_lat},#{remote_long}"
-        x_geo_localization = "#{x_lat},#{x_long}"
-
-        remote_query = Geocoder.search(remote_geo_localization)&.first
-        remote_address = remote_query&.address
-
-        x_query = Geocoder.search(x_geo_localization)&.first
-        x_address = x_query&.address
-
-        ContactUsMailer.contact_us(
-          name: params[:name],
-          email: params[:email],
-          message: params[:message],
-          remote_ip: request.remote_ip,
-          x_forwarded_for: request.env["HTTP_X_FORWARDED_FOR"],
-          remote_geo_localization: remote_geo_localization,
-          x_geo_localization: x_geo_localization,
-          remote_address: remote_address,
-          x_address: x_address
-        ).deliver!
-      rescue => e
-        Rails.logger.info "MAIL ERROR: #{e.message}"
-        Rails.logger.info "BACKTRACE: #{e.backtrace}"
-        ContactUsMailer.contact_us(
-          name: params[:name],
-          email: params[:email],
-          message: params[:message],
-          remote_ip: request.remote_ip,
-          x_forwarded_for: request.env["HTTP_X_FORWARDED_FOR"]
-        ).deliver!
-      end
+      ContactUsMailer.contact_us(
+        name: params[:name],
+        email: params[:email],
+        message: params[:message],
+        headers: headers
+      ).deliver!
 
       flash[:notice] = "Thank you for contacting us. We will get back to you shortly."
       render :index
--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -2,16 +2,11 @@ class ContactUsMailer < ApplicationMailer
   default from: 'ethicalrescueassociation@gmail.com'
   layout 'mailer'
 
-  def contact_us(name:, email:, message:, remote_ip:, x_forwarded_for:, remote_geo_localization: nil, x_geo_localization: nil, remote_address: nil, x_address: nil)
+  def contact_us(name:, email:, message:, headers:)
     @name = name
     @email = email
     @message = message
-    @remote_ip = remote_ip
-    @x_forwarded_for = x_forwarded_for
-    @remote_geo_localization = remote_geo_localization
-    @x_geo_localization = x_geo_localization
-    @remote_address = remote_address
-    @x_address = x_address
+    @headers = headers
     mail(
       to: ['ethicalrescueassociation@gmail.com', 'mishakrul@gmail.com'],
       from: @email,
