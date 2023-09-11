ebdc4ae7cbf3cfc964fdd81f1507a0adf6418b73
mkrul <mishakrul@gmail.com>
2023-09-11 15:56:28 -0400

    get headers

--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -2,11 +2,23 @@ class ContactUsMailer < ApplicationMailer
   default from: 'ethicalrescueassociation@gmail.com'
   layout 'mailer'
 
-  def contact_us(name:, email:, message:, headers:)
+  def contact_us(name:, email:, message:, city:, region:, region_code:, continent:, lat:, long:, metro_code:, zip:, timezone:, ip:, x_request_id:, x_request_start:, sent_at:)
     @name = name
     @email = email
     @message = message
-    @headers = headers
+    @city = city
+    @region = region
+    @region_code = region_code
+    @continent = continent
+    @lat = lat
+    @long = long
+    @metro_code = metro_code
+    @zip = zip
+    @timezone = timezone
+    @ip = ip
+    @x_request_id = x_request_id
+    @x_request_start = x_request_start
+    @sent_at = sent_at
     mail(
       to: ['ethicalrescueassociation@gmail.com', 'mishakrul@gmail.com'],
       from: @email,
