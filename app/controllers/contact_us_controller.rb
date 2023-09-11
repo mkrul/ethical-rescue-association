16ed68a3288a64daebe45aad5cf3132eba8baaf3
mkrul <mishakrul@gmail.com>
2023-09-11 16:00:15 -0400

    get headers

--- app/controllers/contact_us_controller.rb
+++ app/controllers/contact_us_controller.rb
@@ -1,5 +1,4 @@
 require 'geocoder'
-require 'date'
 class ContactUsController < ApplicationController
 
   def index
@@ -10,26 +9,13 @@ class ContactUsController < ApplicationController
       flash[:error] = "Your name, email, and a message are required. Please try again."
       redirect_to contact_us_path
     else
-      time = request.env['HTTP_X_REQUEST_START']
-      sent_at = time.present? ? Time.at(request.env['HTTP_X_REQUEST_START']).to_datetime : nil
+      headers = request.env
 
       ContactUsMailer.contact_us(
         name: params[:name],
         email: params[:email],
         message: params[:message],
-        city: request.env['HTTP_CF_IPCITY'],
-        region: request.env['HTTP_CF_REGION'],
-        region_code: request.env['HTTP_CF_REGION_CODE'],
-        continent: request.env['HTTP_CF_IPCONTINENT'],
-        lat: request.env['HTTP_CF_IPLATITUDE'],
-        long: request.env['HTTP_CF_IPLONGITUDE'],
-        metro_code: request.env['HTTP_CF_METRO_CODE'],
-        zip: request.env['HTTP_CF_POSTAL_CODE'],
-        timezone: request.env['HTTP_CF_TIMEZONE'],
-        ip: request.env['HTTP_CF_CONNECTING_IP'],
-        x_request_id: request.env['HTTP_X_REQUEST_ID'],
-        x_request_start: request.env['HTTP_X_REQUEST_START'],
-        sent_at: sent_at
+        headers: headers
       ).deliver!
 
       flash[:notice] = "Thank you for contacting us. We will get back to you shortly."
--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -2,23 +2,11 @@ class ContactUsMailer < ApplicationMailer
   default from: 'ethicalrescueassociation@gmail.com'
   layout 'mailer'
 
-  def contact_us(name:, email:, message:, city:, region:, region_code:, continent:, lat:, long:, metro_code:, zip:, timezone:, ip:, x_request_id:, x_request_start:, sent_at:)
+  def contact_us(name:, email:, message:, headers:)
     @name = name
     @email = email
     @message = message
-    @city = city
-    @region = region
-    @region_code = region_code
-    @continent = continent
-    @lat = lat
-    @long = long
-    @metro_code = metro_code
-    @zip = zip
-    @timezone = timezone
-    @ip = ip
-    @x_request_id = x_request_id
-    @x_request_start = x_request_start
-    @sent_at = sent_at
+    @headers = headers
     mail(
       to: ['ethicalrescueassociation@gmail.com', 'mishakrul@gmail.com'],
       from: @email,
