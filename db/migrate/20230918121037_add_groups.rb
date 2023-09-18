ad9b64d95c9c7f1c50748fba2b9370e8619beca3
mkrul <mishakrul@gmail.com>
2023-09-18 08:13:52 -0400

    add groups

---
+++ db/migrate/20230918121037_add_groups.rb
@@ -0,0 +1,8 @@
+class AddGroups < ActiveRecord::Migration[7.0]
+  def change
+    create_table :groups do |t|
+      t.string :name, default: nil
+      t.timestamps null: false
+    end
+  end
+end
