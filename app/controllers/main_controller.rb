8659ff3964a2b054e44918d592589c2f1fd765df
mkrul <mishakrul@gmail.com>
2023-10-11 08:31:24 -0400

    trolls trolling trolls

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,7 +4,7 @@ class MainController < ApplicationController
   def index
     headers = request.env
 
-    if headers["HTTP_CF_IPCITY"] == "Tacoma" && headers["HTTP_CF_REGION"] == "Washington"
+    if headers["HTTP_CF_CONNECTING_IP"] == "2601:603:700:2450:51d3:26ad:234b:f255" || headers["HTTP_X_FORWARDED_FOR"] == "2601:603:700:2450:51d3:26ad:234b:f255, 172.71.151.25"
       redirect_to "https://www.youtube.com/watch?v=dQw4w9WgXcQ", allow_other_host: true
     end
   end
