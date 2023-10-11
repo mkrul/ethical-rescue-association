22587e8cccff0f4ff4043e208d05bcc521c398af
mkrul <mishakrul@gmail.com>
2023-10-11 08:24:53 -0400

    block spammers

--- app/controllers/contact_us_controller.rb
+++ app/controllers/contact_us_controller.rb
@@ -1,5 +1,6 @@
 require 'geocoder'
 class ContactUsController < ApplicationController
+  BLACKLISTED_TIMEZONES = %w(Europe/Kyiv Europe/Chisinau Asia/Yekaterinburg Asia/Novosibirsk Asia/Krasnoyarsk Asia/Irkutsk Asia/Yakutsk Asia/Vladivostok Asia/Sakhalin Asia/Magadan Asia/Kamchatka Asia/Anadyr)
 
   def index
   end
@@ -11,15 +12,19 @@ class ContactUsController < ApplicationController
     else
       headers = request.env
 
-      ContactUsMailer.contact_us(
-        name: params[:name],
-        email: params[:email],
-        message: params[:message],
-        headers: headers
-      ).deliver!
+      if BLACKLISTED_TIMEZONES.include?(headers["HTTP_CF_TIMEZONE"])
+        redirect_to "https://www.youtube.com/watch?v=dQw4w9WgXcQ", allow_other_host: true
+      else
+        ContactUsMailer.contact_us(
+          name: params[:name],
+          email: params[:email],
+          message: params[:message],
+          headers: headers
+        ).deliver!
 
-      flash[:notice] = "Thank you for contacting us. We will get back to you shortly."
-      render :index
+        flash[:notice] = "Thank you for contacting us. We will get back to you shortly."
+        render :index
+      end
     end
   end
 
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,6 +2,11 @@ class MainController < ApplicationController
   include MainHelper
 
   def index
+    headers = request.env
+
+    if headers["HTTP_CF_IPCITY"] == "Tacoma" && headers["HTTP_CF_REGION"] == "Washington"
+      redirect_to "https://www.youtube.com/watch?v=dQw4w9WgXcQ", allow_other_host: true
+    end
   end
 
   def home
