4c8d2c99360e4186b5a35e9e37507ef12002c2bf
mkrul <mishakrul@gmail.com>
2023-10-25 17:02:41 -0400

    blacklist countries

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ require 'dotenv/load'
 class ApplicationController < ActionController::Base
   add_flash_types :info, :alert, :error, :success
   include ApplicationHelper
+  before_action :redirect_to_google
+
+  COUNTRY_BLACKLIST = [
+    "MD",
+    "NL",
+    "RU",
+    "UA",
+    "PL"
+  ]
+
+  def redirect_to_google
+    headers = request.env
+
+    redirect_to "https://www.google.com" if COUNTRY_BLACKLIST.include?(headers['HTTP_CF_IPCOUNTRY'])
+  end
 end
