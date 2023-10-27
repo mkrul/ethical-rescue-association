de919355033622f7022761c25e48647b6de532b4
mkrul <mishakrul@gmail.com>
2023-10-27 10:25:58 -0400

    ERA-168 add paypal integration

--- app/controllers/application_funnel_controller.rb
+++ app/controllers/application_funnel_controller.rb
@@ -5,7 +5,11 @@ class ApplicationFunnelController < ApplicationController
   end
 
   def show
-    debugger
+    redirect_to root_url unless current_user && current_user.developer?
+
+    Rails.logger.info "\n\n\nPOTATO:\n\n\n"
+    Rails.logger.info request.params
+    Rails.logger.info request
   end
 
 end
