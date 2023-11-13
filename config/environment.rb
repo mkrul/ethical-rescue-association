62f6eac3de3541ef237903617a9842493d18302b
mkrul <mishakrul@gmail.com>
2023-11-13 08:05:53 -0500

    update rollbar env settings

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,26 @@
 # Load the Rails application.
 require_relative "application"
 require File.expand_path('../application', __FILE__)
+require File.expand_path('../rollbar', __FILE__)
 
-Rails.application.initialize!
+if Rails.env.production?
+  notify = ->(e) do
+    begin
+      Rollbar.with_config(use_async: false) do
+        Rollbar.error(e)
+      end
+    rescue
+      Rails.logger.error "Synchronous Rollbar notification failed.  Sending async to preserve info"
+      Rollbar.error(e)
+    end
+  end
+
+  begin
+    Rails.application.initialize!
+  rescue Exception => e
+    notify.(e)
+    raise
+  end
+else
+  Rails.application.initialize!
+end
