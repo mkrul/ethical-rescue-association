6a6b2086c0074830952231afbb79a6f5597aeaaf
mkrul <mishakrul@gmail.com>
2023-11-17 08:01:00 -0500

    ERA-210 integrate aws, allow delete organizations

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,24 @@
 import { Application } from "@hotwired/stimulus"
+import * as ActiveStorage from "@rails/activestorage"
+import { Controller } from "@hotwired/stimulus"
+
+ActiveStorage.start()
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
+
+export default class extends Controller {
+  static values = { message: String };
+
+  confirm(event) {
+    if (!(window.confirm(this.messageValue))) {
+      event.preventDefault();
+      event.stopImmediatePropagation();
+    };
+  };
+}
 
 export { application }
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,8 @@
 // Import and register all your controllers from the importmap under controllers/*
 
 import { application } from "controllers/application"
+import ConfirmationController from "./confirmation_controller"
+application.register("confirmation", ConfirmationController)
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
