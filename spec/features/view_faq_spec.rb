dfa82857bb9962f8446f3ee8383b6736982d9f98
mkrul <mishakrul@gmail.com>
2023-11-17 22:40:55 -0500

    check http cf region

--- spec/features/view_faq_spec.rb
+++ spec/features/view_faq_spec.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../spec_helper", __FILE__)
 RSpec.describe "View FAQ", type: :feature, js: true do
   scenario 'clicking on an faq' do
     visit faq_path
-    page.find('h5', text: 'Does ERA support animal rights?').click
+    page.find('h5', text: 'How can a rescue apply for ERA accreditation?').click
 
-    expect(page).to have_content('Animal rights advocacy is based in the belief that')
+    expect(page).to have_content('Begin your application by clicking')
   end
 end
