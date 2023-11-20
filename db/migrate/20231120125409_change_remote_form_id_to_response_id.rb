325918cbb0665c59fd32d2fe7b8f783ecf41967c
mkrul <mishakrul@gmail.com>
2023-11-20 08:00:23 -0500

    change response_id to remote_form_id

--- db/migrate/20231120125409_change_remote_form_id_to_response_id.rb
+++ db/migrate/20231120125409_change_remote_form_id_to_response_id.rb
@@ -1,5 +1,5 @@
 class ChangeRemoteFormIdToResponseId < ActiveRecord::Migration[7.0]
   def change
-    rename_column :application_submissions, :response_id, :response_id
+    rename_column :application_submissions, :remote_form_id, :response_id
   end
 end
