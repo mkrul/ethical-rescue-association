90e1ecaad6d7abf9260a8d95d85f977862b40eb1
mkrul <mishakrul@gmail.com>
2024-01-15 10:37:22 -0500

    change names

--- spec/features/view_board_member_bio_spec.rb
+++ spec/features/view_board_member_bio_spec.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../spec_helper", __FILE__)
 RSpec.describe "View board member bio", type: :feature, js: true do
   scenario 'view board member bio' do
     visit what_is_era_path
-    photo = page.find("img[alt='Angela Curry']").click
+    photo = page.find("img[alt='Alexis Coleman']").click
 
-    expect(page).to have_content('Angela’s passion for working with animals')
+    expect(page).to have_content('Alexis’s passion for working with animals')
   end
 end
