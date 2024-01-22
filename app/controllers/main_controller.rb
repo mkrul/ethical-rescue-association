8bddfc02a611426bba1eaf4da2e91fc0622f386f
mkrul <mishakrul@gmail.com>
2024-01-22 06:32:44 -0500

    log testcred

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,18 +7,12 @@ class MainController < ApplicationController
   def index
     api_key = Rails.application.credentials[Rails.env.to_sym][:datadog][:dd_api_key]
     @client ||= Dogapi::Client.new(api_key)
+    debugger
     testcred = Rails.application.credentials[Rails.env.to_sym][:mycred][:derp]
     Rails.logger.info testcred
 
     Rails.logger.info "DataDog client initialized"
 
-    @client.emit_point('test.metric', 100)
-  end
-
-  def home
-    api_key = Rails.application.credentials[Rails.env.to_sym][:datadog][:dd_api_key]
-    @client ||= Dogapi::Client.new(api_key)
-    Rails.logger.info "DataDog client initialized"
     @client.emit_point('test.metric', 100)
   end
 
