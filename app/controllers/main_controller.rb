fdd1ad68b708af95792a215d1aad61b5d74b115d
mkrul <mishakrul@gmail.com>
2024-01-22 07:07:47 -0500

    adjust spacing

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,7 +5,7 @@ class MainController < ApplicationController
   include MainHelper
 
   def index
-    api_key = Rails.application.credentials[Rails.env.to_sym][:datadog][:dd_api_key]
+    api_key = Rails.application.credentials[Rails.env.to_sym][:datadog][:dd_app_key]
     @client ||= Dogapi::Client.new(api_key)
 
     Rails.logger.info "DataDog client initialized"
